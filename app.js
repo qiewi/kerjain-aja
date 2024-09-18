85f6a45ce5889a8d93827b34c32938bcdda9858a
qiewi <105619444+qiewi@users.noreply.github.com>
2024-09-18 13:02:11 +0700

    todo list update

--- app.js
+++ app.js
@@ -14,8 +14,22 @@ function addTodo() {
     
     if(todoText.length > 0) {
         allTodos.push(todoText);
-        createTodoItem(todoText);
+        updateTodoList();
         todoInput.value = "";
     }
     
+}
+
+function updateTodoList() {
+    todoListUL.innerHTML = "";
+    allTodos.forEach((todo, todoIndex) => {
+        todoItem = createTodoItem(todo, todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo) {
+    const todoLI = document.createElement("li");
+    todoLI.innerText = todo;
+    return todoLI;
 }
