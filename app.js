721e1c0f97c5c86e64b82c8c1fb301f507d80797
qiewi <105619444+qiewi@users.noreply.github.com>
2024-09-18 13:10:25 +0700

    ft: saving & getting todos from local storage

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const todoForm = document.querySelector('form');
 const todoInput = document.getElementById('todo-input');
 const todoListUL = document.getElementById('todo-list');
 
-let allTodos = [];
+let allTodos = getTodos();
+updateTodoList();
 
 todoForm.addEventListener('submit', function(e){
     e.preventDefault();
@@ -15,6 +16,7 @@ function addTodo() {
     if(todoText.length > 0) {
         allTodos.push(todoText);
         updateTodoList();
+        saveTodos();
         todoInput.value = "";
     }
     
@@ -50,4 +52,14 @@ function createTodoItem(todo, todoIndex) {
     `
 
     return todoLI;
+}
+
+function saveTodos() {
+    const todosJson = JSON.stringify(allTodos);
+    localStorage.setItem("todos", todosJson);
+}
+
+function getTodos() {
+    const todos = localStorage.getItem("todos") || "[]";
+    return JSON.parse(todos);
 }
